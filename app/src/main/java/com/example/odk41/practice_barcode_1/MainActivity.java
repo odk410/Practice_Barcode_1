5d399ca794a27beb234216830dffd27af784508e
unknown <odk410@naver.com>
2018-02-22 15:28:29 +0900

    Initial commit

---
+++ app/src/main/java/com/example/odk41/practice_barcode_1/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.odk41.practice_barcode_1;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //추가부분 바코드 인식 기능
+    public void scanBarcode(View view) {
+
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        //카메라 가로, 세로 설정
+        integrator.setOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        integrator.addExtra("PROMPT_MESSAGE", "바코드를 사각형 안에 비춰주세요");
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            // This is important, otherwise the result will not be passed to the fragment
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
